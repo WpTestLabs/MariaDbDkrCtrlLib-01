fab4cd9e00ac9bfedd5a4b3211c696c4886c3e9b
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-08-20 16:31:36 -0400

    Update standardInitDbGX.sh

--- standardInitDbGX.sh
+++ standardInitDbGX.sh
@@ -6,7 +6,7 @@ ping () { echo ">>>>>>>> mysql ping: \`mysqladmin -uroot ping\`"; }
 pLkDnSQL () {
 cat <<EOF
 USE mysql;
-UPDATE user set password=PASSWORD("\`cat /srv/.msRoot.pwa\`") where User='root';
+UPDATE user set password=PASSWORD("\`cat /srv/.msRoot.pwa\`") where User='root' AND Host = 'localhost';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE test;
@@ -14,7 +14,11 @@ DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
 FLUSH PRIVILEGES;
 EOF
 }
+#UPDATE user set password=PASSWORD("\`cat /srv/.msRoot.pwa\`") where User='root'    @'localhost';
 
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_password';
+#UPDATE mysql.user SET authentication_string = PASSWORD('new_password') WHERE User = 'root' AND Host = 'localhost';
+#                       ^^^ for mysql -- mariadb still password
 #msg "[MariaDB] Starting: standardInitDb.sh - as: \`whoami\`  pwd: \$PWD"
 cat >/root/.my.cnf <<<!include /srv/.myRtPW.cnf
 chown $MdbUsr:$MdbGrp /root/.my.cnf && chmod 440 /root/.my.cnf
