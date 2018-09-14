4d5b54c0249b7fadc58d489a7729239cf266b81a
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-09-14 12:22:14 -0400

    Create install-01.sh

---
+++ install-01.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#   echo "https://github.com/WpTestLabs/MariaDbDkrCtrlLib-01/install-o1.sh"
+
+install -pm 640 -o root -g root -t $SrvLib cfMariaDbDkrLib.sh  iMariaDbKnHX.sh 
+install -pm 740 -o root -g root -t $SrvBin nMariaDbKn.sh nMariaDbKnDef.sh
+pth=$SrvLib/MariaDB/GX/;    mkdir -p $pth && \
+  install -pm 640 -o root -g root -t $pth stMariaDbSvrGX.sh standardInitDbGX.sh
+ln -srf $SrvBin/cfKnCli.sh $SrvBin/sql
+ln -srf $SrvBin/cfKnCli.sh $SrvBin/mdb
