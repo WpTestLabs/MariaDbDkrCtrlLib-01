aab763027a7920ea3fae9c62978b8fa9d17284c0
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-09-29 17:59:59 -0400

    Add wkStpRmtSqlBuGX-01.sh

--- install-01.sh
+++ install-01.sh
@@ -4,6 +4,6 @@
 install -pm 640 -o root -g root -t $SrvLib cfMariaDbDkrLib.sh  iMariaDbKnHX.sh 
 install -pm 740 -o root -g root -t $SrvBin nMariaDbKn.sh nMariaDbKnDef.sh
 pth=$SrvLib/MariaDB/GX/;    mkdir -p $pth && \
-  install -pm 640 -o root -g root -t $pth stMariaDbSvrGX.sh standardInitDbGX.sh
+  install -pm 640 -o root -g root -t $pth stMariaDbSvrGX.sh standardInitDbGX.sh wkStpRmtSqlBuGX-01.sh
 ln -srf $SrvBin/cfKnCli.sh $SrvBin/sql
 ln -srf $SrvBin/cfKnCli.sh $SrvBin/mdb
