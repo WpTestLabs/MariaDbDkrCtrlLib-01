a193fa88704939d7a8a22bdbdad08009f3844012
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-09-30 11:16:58 -0400

    Update install-01.sh

--- install-01.sh
+++ install-01.sh
@@ -3,7 +3,8 @@
 
 install -pm 640 -o root -g root -t $SrvLib cfMariaDbDkrLib.sh  iMariaDbKnHX.sh 
 install -pm 740 -o root -g root -t $SrvBin nMariaDbKn.sh nMariaDbKnDef.sh
-pth=$SrvLib/MariaDB/GX/;    mkdir -p $pth && \
+pth=$SrvLib/MariaDB/GX/ &&   mkdir -p $pth && \
   install -pm 640 -o root -g root -t $pth stMariaDbSvrGX.sh standardInitDbGX.sh wkStpRmtSqlBuGX-01.sh
+echo "After MariaDB install GX... should be clear>> pfn: $pfn"
 ln -srf $SrvBin/cfKnCli.sh $SrvBin/sql
 ln -srf $SrvBin/cfKnCli.sh $SrvBin/mdb
