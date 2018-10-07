3858feee632960631d2c1b439b8cf901636146fc
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-07 12:44:15 -0400

    Update sqlHeartBeatGX.sh

--- sqlHeartBeatGX.sh
+++ sqlHeartBeatGX.sh
@@ -3,35 +3,42 @@
 set -e
 # msg () { echo "$@" >> /srv/run/wkFlo/hstWkFloRcv.fifo; }
   msg "TL [SQL] Start: sqlHeartBeatGX.sh"
-  msg  "# [SQL] Start: sqlHeartBeatGX.sh"
+  msg  "# [SQL] Start: sqlHeartBeatGX.sh - SrvGP: $SrvGP"
 
 export SleepCnt=10
 . $SrvGP/guestEnv.sh
+msg  "# [SQL] sqlHeartBeatGX.sh - After: . $SrvGP/guestEnv.sh"
 
 cd $SrvGP
   
 chkKids () { msg "# [SQL] Start: sqlHeartBeatGX.chkKids()";
    #xx ps -ppid $$ -o pid,comm,state --no-headers | \ Alpine BusyBox: Missing options 
    	ps -o ppid= -o pid= -o comm= -o stat= | \
-	while read ln; do msg "#[sql] chkKids() - $ln"; done
+	while read ln; do msg "# [sql] chkKids() - ps:  $ln"; done
 }
+msg  "# [SQL] sqlHeartBeatGX.sh - After: chkKids()"
+
 chkQ () { local lst=`ls $BatGP/inQ`
-	if [[ -n "$lst" ]]; then 
+    if [[ -n "$lst" ]]; then 
 	    msg "# [SQL] chkQ() - Found: $lst"
-		if [[ -z "`ls $BatGP/pending`" ]]
+	if [[ -z "`ls $BatGP/pending`" ]]
 			msg "# [SQL] Moving Q'd files to pending"
             mv $BatGP/inQ/* $BatGP/pending/
 			./sqlTskRnrGX.sh
         else  msg "# [SQL] Already have pending files to clear first." 
         fi	
-	fi
+    fi
 }
+msg  "# [SQL] sqlHeartBeatGX.sh - After: chkQ()"
 sqlPing () { local s=`mysqladmin ping` r=$? ; echo "$r  $s"; }
+msg  "# [SQL] sqlHeartBeatGX.sh - After: sqlPing()"
 
 onBeat () {   msg "SqlHB `sqlPing`";
     chkKids;  chkQ;	
 }
+msg  "# [SQL] sqlHeartBeatGX.sh - After: onBeat()"
+
 heartBeat () {	while true; do onBeat; sleep $SleepCnt; done; }
 
-msg "# [SQL]  sqlHeartBeat.sh - Start: heartBeat()"
+msg "# [SQL]  sqlHeartBeat.sh - Calling: heartBeat()"
 heartBeat
