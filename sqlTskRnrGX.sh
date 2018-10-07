dac42f9599ccc42e468a126d1559dbbdeb8444f6
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-07 17:59:41 -0400

    Update sqlTskRnrGX.sh

--- sqlTskRnrGX.sh
+++ sqlTskRnrGX.sh
@@ -2,10 +2,20 @@
 	msg "# [SQL] Start - $SrvGP/sqlTskRnrGX.sh"
 set -e
 
+diagFileN () { msg "# [SQL] ----------------------------"
+    msg "# [SQL] diagFileN() - Full Input File (FQPFN): $1"
+    msg "# [SQL] diagFileN() - Filename without Path: " $(basename "$1")
+    msg "# [SQL] diagFileN() - Filename without Extension: " ${1%.*}
+    msg "# [SQL] diagFileN() - File Extension without Name: " ${1##*.}
+}
+
+
+
 while true; do   lst=`ls $BatGP/pending`
   if [[ -n "lst" ]]; then
     for f in $lst; do  msg "# [SQL] sqlTskRnr - Next file: $f"
 		  mv $BatGP/pending/$f  $BatGP/cur
+	diagFileN $f
       sleep 3;
     done
   else  msg "# [SQL] sqlTskRnrGX.sh - Nothing in Q"
