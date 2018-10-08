2a4a15375b5406f127f113d6470377159c418fb9
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-08 15:11:16 -0400

    Update sqlTskRnrGX.sh

--- sqlTskRnrGX.sh
+++ sqlTskRnrGX.sh
@@ -18,7 +18,7 @@ while true; do   lst=`ls $BatGP/pending`
   if [[ -n "lst" ]]; then
     for f in $lst; do  msg "# [SQL] sqlTskRnr - Next file: $f"
 		  mv $BatGP/pending/$f  $BatGP/cur
-	diagFileN $f;  rnFl $BatGP/cur/$f
+	diagFileN $f;  rnFl $BatGP/cur/$f > $BatGP/cur/tstRnr-${f}.log
 
       sleep 3;
     done
