22efeb85a3fca5a90e0901f2551376ee308d66ad
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-21 14:14:48 -0400

    Update cfMariaDbDkrLib.sh

--- cfMariaDbDkrLib.sh
+++ cfMariaDbDkrLib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "start: \$SrvLib/cfMariaDbDkrLib.sh"
 
-.	$SrvLib/cfDkrCtrlLib.sh
+. $SrvLib/cfDkrCtrlLib.sh
 
 databases () { docker exec -it $CID mysql -uroot -e 'SHOW DATABASES;'; }
 databases-ss () { docker exec -it $CID mysql -uroot -ss -e 'SHOW DATABASES;'; }
@@ -16,10 +16,10 @@ print-defaults () { docker exec -it $CID mysql --print-defaults -uroot ; }
 sqlStatus () { docker exec -it $CID mysqladmin status; }
 
 Run () {
-	echo ">>> sql Run() -- KnBasHP: $KnBasHP"
+    echo ">>> sql Run() -- KnBasHP: $KnBasHP"
 #tt	tree -ACa $KnBasHP
-	. $SrvLib/iMariaDbKnHX.sh 
-	_Run
+    . $SrvLib/iMariaDbKnHX.sh 
+    _Run
 }
 Stop () { DkrStop; }
 
@@ -35,34 +35,34 @@ QFile () {	echo "  QFile() - PFN: $1 -- `basename $1`"
 }
 
 DoSqlFile () {	echo "  DoSqlFile() - PFN: $1 -- `basename $1`"
-	local pfn=$1  baseN=`basename $1;`;  shift;	
-	local qInHP=$KnBasHP/srv/Q/In;		mkdir -p $qInHP;
-	if [[ -e $pfn ]]; then
-		echo "  vvvv  Found file  vvvv";  cat $pfn;
-		cp -p $pfn $qInHP;  		ls -al $qInHP;  cat $qInHP/$baseN
-		docker exec  $CID mysql -uroot -e "source /srv/Q/In/$baseN;";
+    local pfn=$1  baseN=`basename $1;`;  shift;	
+    local qInHP=$KnBasHP/srv/Q/In;		mkdir -p $qInHP;
+    if [[ -e $pfn ]]; then
+        echo "  vvvv  Found file  vvvv";  cat $pfn;
+        cp -p $pfn $qInHP;         ls -al $qInHP;  cat $qInHP/$baseN
+        docker exec  $CID mysql -uroot -e "source /srv/Q/In/$baseN;";
 
-		rm $qInHP/$baseN;		ls -al $qInHP;
-	else
-		echo "  *** SQL File Missing - $pfn ***"
-	fi 
+        rm $qInHP/$baseN;          ls -al $qInHP;
+    else
+        echo "  *** SQL File Missing - $pfn ***"
+    fi 
 }
 
 DoShFile () {	echo "  DoShFile() - PFN: $1 -- `basename $1`"
-	local hpfn=$1  baseN=`basename $1;`;  shift;
-	local knSrvTmpGP=/srv/tmp
-	local knSrvTmpHP=$KnBasHP/$knSrvTmpGP;		mkdir -p $knSrvTmpHP;
-	if [[ -e $hpfn ]]; then
-		echo "  vvvv  Found file  vvvv";  cat $hpfn;
-		cp -p $hpfn $knSrvTmpHP;  	
-		    ls -al $knSrvTmpHP;  cat $knSrvTmpHP/$baseN
-		docker exec  $CID $knSrvTmpGP/$baseN $@;
+    local hpfn=$1  baseN=`basename $1;`;  shift;
+    local knSrvTmpGP=/srv/tmp
+    local knSrvTmpHP=$KnBasHP/$knSrvTmpGP;		mkdir -p $knSrvTmpHP;
+    if [[ -e $hpfn ]]; then
+        echo "  vvvv  Found file  vvvv";  cat $hpfn;
+        cp -p $hpfn $knSrvTmpHP;  	
+            ls -al $knSrvTmpHP;  cat $knSrvTmpHP/$baseN
+        docker exec  $CID $knSrvTmpGP/$baseN $@;
 
-		rm $knSrvTmpHP/$baseN;			ls -al $knSrvTmpHP;
-	else
-		echo "  *** sh File Missing - $hpfn ***"
-	fi 
-}
+        rm $knSrvTmpHP/$baseN;			ls -al $knSrvTmpHP;
+    else
+        echo "  *** sh File Missing - $hpfn ***"
+    fi 
+} 
 
 xDbDmp () { echo "[Hst] cfMariaDbDkrLib.DbDmp - RunGP: $RunGP  (1)";
     On; echo "[Hst] cfMariaDbDkrLib.DbDmp - RunGP: $RunGP  (2)";
