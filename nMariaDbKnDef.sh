025345bbfd87799eff6f3dc679a2ad0fdc2ab1ef
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2019-02-10 09:43:23 -0500

    Update nMariaDbKnDef.sh

--- nMariaDbKnDef.sh
+++ nMariaDbKnDef.sh
@@ -36,4 +36,5 @@ export FQKnRtN=\${KnGrpN}._.\${KnRtN}
 export DkrRtLbls=" --label stk=\$SvrStkN  --label grp=\$KnGrpN --label kanT=\$KnPkgN "
     #qq export DkrVolGrp=" -v $KnEtcHP/:/etc/$KnPkgN/:ro  -v $KnLogHP/:/var/log/$KnPkgN  
 export DkrVolGrp=" -v \$KnBasHP/srv:/srv "
+[[ -z "$GoDevPth" ]] || DkrVolGrp="$DkrVolGrp  -v $GoDevPth:/srv/go"
 EOF
